131fa21f90df326e99506e942c00bb9032a77901
tushart681 <tushart681@gmail.com>
2022-09-30 01:44:15 +0600

    final update7

--- src/components/blogs/Blogs.js
+++ src/components/blogs/Blogs.js
@@ -5,11 +5,11 @@ const Blogs = () => {
         <div className='flex flex-col gap-8'>
             <div className='p-4 text-lg border'>
                 <p className='text-xl font-semibold'>Question1: How does React works?</p>
-                <p>Answer:</p>
+                <p>Answer: ReactJS divides the UI into isolated reusable pieces of code known as components. React components work similarly to JavaScript functions as they accept arbitrary inputs called properties or props. It's possible to have as many components as necessary without cluttering your code.</p>
             </div>
             <div className='p-4 text-lg border'>
                 <p className='text-xl font-semibold'>Question2: Difference between props and state</p>
-                <p>Answer:</p>
+                <p>Answer: Props are used to pass data from one component to another. The state is a local data storage that is local to the component only and cannot be passed to other components.</p>
             </div>
             <div className='p-4 text-lg border'>
                 <p className='text-xl font-semibold'>Question3: Usage of useEffect without calling api</p>
